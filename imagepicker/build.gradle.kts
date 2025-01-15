a3be664211d630f44b1f09aa2365377389d7bf9a
Eunggyo <eungkyo89@gmail.com>
2025-01-15 17:18:46 +0900

    Coil dependency 추가

--- imagepicker/build.gradle.kts
+++ imagepicker/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("io.coil-kt.coil3:coil-compose:3.0.4")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
