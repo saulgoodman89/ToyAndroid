9f54adfe1b1271c43debb400a9fb22ab3860e0b6
Eunggyo <eungkyo89@gmail.com>
2025-01-15 22:05:50 +0900

    LazyVerticalGrid로 선택한 사진 리스팅. 1) 이미지 리스팅은 되었으나 이미지 원본의 크기에 따라 여백이 생기는 등 균일하지 않게 발생함. 2) AsyncImage Composable의 contentScale , modifier.aspectRatio()로 문제 해결. 실제 갤럭시 갤러리 앱의 경우 사진 크기와 상관없이 모두 동일한 크기로 표시되었으나 사진 크기에 따라 확대되어 보어 표시됨. contentScale이 이런 역활을 하는데 원본 이미지 비율은 유지하면서 주어진 공간을 채우기 위해 이미지 중앙을 기준으로 잘라낸다.

--- imagepicker/src/main/java/com/keg/imagepicker/ImagePickerScreen.kt
+++ imagepicker/src/main/java/com/keg/imagepicker/ImagePickerScreen.kt
@@ -14,12 +14,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -34,7 +40,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layout
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.size.Size
 
 @Composable
 fun ImagePickerScreen(viewModel: ImagePickerViewModel = ImagePickerViewModel()) {
@@ -79,37 +90,43 @@ fun ImagePickerScreen(viewModel: ImagePickerViewModel = ImagePickerViewModel()) 
         // 선택한 이미지와 순서를 text로 표시한다.
         if (images.isNotEmpty()) {
             Spacer(modifier = Modifier.height(16.dp))
-            LazyColumn(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(16.dp)
-            ) {
-                itemsIndexed(images) { index, uri ->
-                    SelectedImageItem(uri = uri, order = index + 1)
-                }
-            }
+           LazyVerticalGrid(
+               columns = GridCells.Fixed(3),
+               modifier = Modifier.fillMaxWidth()
+              //     .aspectRatio(1f)
+           ) {
+               itemsIndexed(images) { index, uri ->
+                   SelectedImageItem(uri = uri)
+               }
+           }
         }
     }
 }
 
 @Composable
-fun SelectedImageItem(uri: Uri, order: Int) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(50.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        // 순서 번호
-        Text(
-            text = "$order",
-            modifier = Modifier.padding(end = 16.dp)
-        )
+fun SelectedImageItem(uri: Uri) {
+        AsyncImage(model = uri,
+            contentDescription = "Selected picture",
+            /*
+                contentScale : 컴포저블에서 이미지 스케일링 방식을 지정.
+                원본 이미지 비율은 유지하면서 주어진 공간을 채우기 위해 이미지를 확대 하거나 축소 ex) Gallery
+                주어진 공간을 위해 이미지의 중앙을 기준으로 잘라낸다.
 
-        // uri 표시
-        Text(
-            text = "${uri.toString()}"
+                다른 옵션
+                ContentScale.Fit : 이미지에 주어진 공간에 맞게 축소,확대 하지만 이미지 비율 유지.
+                ContentScale.FillBounds : 이미지 비율을 무시하고 주어진 공간을 완전히 채우도록 스캐일링.
+                ContentScale.Inside : 주어진 공간 안에 완전히 들어가도록 축소,확대 하지만 비율 유지. 이미지가 주어진 공간을 완전히
+                채우지 않을 수 있다.
+
+             */
+            //contentScale = ContentScale.Fit, 실제 사용 시 공간을 완전히 채우지 않는다.
+           // contentScale = ContentScale.FillBounds,  실제 사용 시 공간은 채우지만 확대 되어 보임.
+         //   contentScale = ContentScale.Inside, 공간을 채우지 못함.
+           contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1f) // Composable의 가로세로 비율을 정한다. 1f : 정사각형
         )
-    }
 }
 
 @Composable
